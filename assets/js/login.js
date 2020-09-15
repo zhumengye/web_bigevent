081d6240d90e4a73ded2631a5ccb9f7ca1d6aeae
ameng <zhumengye.@itcast.cn>
2020-09-15 12:42:29 +0800

    完成了登录和注册功能

---
+++ assets/js/login.js
@@ -0,0 +1,62 @@
+$(function () {
+  // 点击“去注册账号”的链接
+  $('#link_reg').on('click', function () {
+    $('.login-box').hide()
+    $('.reg-box').show()
+  })
+
+  // 点击“去登录”的链接
+  $('#link_login').on('click', function () {
+    $('.login-box').show()
+    $('.reg-box').hide()
+  })
+
+  // 从 layui 中获取 form 对象：
+  var form = layui.form
+  form.verify({
+    pwd: [
+      /^[\S]{6,12}$/
+      , '密码必须6到12位，且不能出现空格'
+    ],
+    repwd: function (value) {
+      var pwd = $('.reg-box [name=password]').val()
+      if (pwd !== value) {
+        return '两次密码不一致！'
+      }
+    }
+  })
+
+  $('#form_reg').on('submit', function (e) {
+    e.preventDefault()
+    var data = {
+      username: $('.reg-box [name=username]').val(),
+      password: $('.reg-box [name=password]').val()
+    }
+    $.post('/api/reguser', data, function (res) {
+      if (res.status !== 0) {
+        return layer.msg(res.message)
+      }
+      layer.msg('注册成功，请登录！')
+      $('#link_login').click()
+    })
+  })
+
+  $('#form_login').submit(function (e) {
+    e.preventDefault()
+    $.ajax({
+      method: 'POST',
+      url: '/api/login',
+      data: $(this).serialize(),
+      success: function (res) {
+        if (res.status !== 0) {
+          return layer.msg('登录失败！')
+        }
+        layer.msg('登录成功！')
+        // 将登录成功得到的 token 字符串，保存到 localStorage 中
+        localStorage.setItem('token', res.token) 
+        location.href = './index.html'
+      }
+    })
+  })
+
+})
